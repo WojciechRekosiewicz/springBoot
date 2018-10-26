d4a21566c72171eab0d8af28976ce5bf3954323f
WojciechRekosiewicz <wojtek24a@gmail.com>
2018-10-26 23:04:31 +0200

    test works

--- src/main/java/com/wojtek/Controller/StudentController.java
+++ src/main/java/com/wojtek/Controller/StudentController.java
@@ -2,16 +2,22 @@ package com.wojtek.Controller;
 
 import com.wojtek.Entity.Student;
 import com.wojtek.Service.StudentsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 
 
 @RestController
+@RequestMapping("/students")
 public class StudentController {
 
+    @Autowired
     private StudentsService studentsService;
 
+    @RequestMapping(method = RequestMethod.GET)
     public Collection<Student> getAllStudents(){
         return studentsService.getAllStudents();
     }
--- src/main/java/com/wojtek/Service/StudentsService.java
+++ src/main/java/com/wojtek/Service/StudentsService.java
@@ -2,6 +2,7 @@ package com.wojtek.Service;
 
 import com.wojtek.Dao.StudentDao;
 import com.wojtek.Entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
@@ -11,6 +12,7 @@ import java.util.Collection;
 @Service
 public class StudentsService {
 
+    @Autowired
     private StudentDao studentDao;
 
     public Collection<Student> getAllStudents(){
